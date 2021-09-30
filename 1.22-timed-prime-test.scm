f38de74d9298c8ca242336d0dc501ce289aab365
Quan Tong <quanta.linux@gmail.com>
2021-09-30 09:11:41 +0700

    1.22: timed prime test

---
+++ 1.22-timed-prime-test.scm
@@ -0,0 +1,43 @@
+#lang sicp
+
+(define (prime? n)
+    (= (smallest-divisor n) n))
+
+(define (smallest-divisor n)
+    (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+        ((devides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (square x)
+    (* x x))
+
+(define (devides? a b)
+    (= (remainder b a) 0))
+
+(define (timed-prime-test n)
+    (newline)
+    (display n)
+    (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+    (if (prime? n)
+        (report-time (- (runtime) start-time))))
+
+(define (report-time elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
+
+(define (search-for-primes start end)
+    (if (even? start)
+        (search-for-primes (+ start 1) end)
+        (cond ((< start end)
+            (timed-prime-test start)
+            (search-for-primes (+ start 2) end)))))
+
+(define (even? n)
+    (= (remainder n 2) 0))
+
+(search-for-primes 90000 99999)
